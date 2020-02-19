d8a49acb951a3e0d03ba4e6635d55a6688014972
Sam Hunter <shunter@gmail.com>
2020-02-18 19:57:42 -0800

    steps towards a real pipeline

--- 01-build_hts.py
+++ 01-build_hts.py
@@ -20,13 +20,14 @@ for l in open(primerf, 'r'):
         continue
     l2 = l.strip().split()
     if len(l2) == 6:
-        sample = primerf.replace(".csv", '') + "_" + l2[header.index('SampleID')] + "_" + l2[header.index('Primer1ID')] + "_" + l2[header.index('Primer2ID')]
+        #sample = primerf.replace(".csv", '') + "_" + l2[header.index('SampleID')] + "_" + l2[header.index('Primer1ID')] + "_" + l2[header.index('Primer2ID')]
+        sample = l2[header.index('SampleID')]
         logf = './01-PrimerTrim/' + sample + '.log'
         prefix = './01-PrimerTrim/' + sample
-        cmd = "../HTStream/build/bin/hts_Primers -d 0 -l 2 -e 0 -r 2 -x -P " + l2[header.index('Primer1Seq')]
+        cmd = "../HTStream/build/bin/hts_Primers -d 0 -l 0 -e 0 -r 2 -x -P " + l2[header.index('Primer1Seq')] + "TGGGG"
         cmd += " -Q " + l2[header.index('Primer2Seq')] + " -1 " + r1f + " -2 " + r1r + " -L " + logf + " | "
-        cmd += "../HTStream/build/bin/hts_SeqScreener -C -x .01 -k 21 -s abberant_LC.fasta -AL " + logf + " | "
-        cmd += "../HTStream/build/bin/hts_QWindowTrim -n -q 20 -m 250 -AL " + logf + " | "
-        cmd += "../HTStream/build/bin/hts_Overlapper -AL " + logf + " -m 400 -f " + prefix + '\n'
+        cmd += "../HTStream/build/bin/hts_SeqScreener -C -r -x .01 -k 21 -s abberant_LC.fasta -AL " + logf + " | "
+        cmd += "../HTStream/build/bin/hts_QWindowTrim -l -n -q 10 -m 260 -AL " + logf + " | "
+        cmd += "../HTStream/build/bin/hts_Overlapper -AL " + logf + " -m 385 -f " + prefix + '\n'
         outf.write(cmd)
 outf.close()
