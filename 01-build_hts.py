bb31447d440c31b25d1a7badb57d14e1150bcc25
Sam Hunter <shunter@gmail.com>
2020-11-18 08:57:42 -0800

    Updates to support 4 primers

--- 01-build_hts.py
+++ 01-build_hts.py
@@ -9,23 +9,19 @@ cmdf = sys.argv[4]
 
 #r1 = glob('./00-RawData/*_R1_*.fastq.gz')[0]
 #r2 = r1.replace('_R1_', '_R2_')
-#primerf = '1_Short_primers.csv'
+#primerf = 'TSO_demux_primers.csv'
 #cmdf = '01-trim-1_Short_CSP.sh'
 
-# r1f = './00-RawData/2_Long_CSP_Pool_S2_L001_R1_001.fastq.gz'
-# r1r = './00-RawData/2_Long_CSP_Pool_S2_L001_R2_001.fastq.gz'
-# primerf = '2_Long_primers.csv'
-# cmdf = '01-trim-2_Long_CSP.sh'
 
-# python 01-build_hts.py ./00-RawData/Trimmer6_S22_L001_R1_001.fastq.gz \
-#     ./00-RawData/Trimmer6_S22_L001_R2_001.fastq.gz \
-#     1_Short_primers.csv 01-trim-1_Short_CSP.sh
+# python3 01-build_hts.py ./00-RawData/R1_250k.fastq.gz \
+#     ./00-RawData/R2_250k.fastq.gz \
+#     TSO_demux_primers.csv 01-trim-1_Short_CSP.sh
 
 os.system('mkdir -p 01-PrimerTrim')
 
 outf = open(cmdf, 'w')
 
-htspath = "../../HTStream/build/bin/"
+htspath = "../../HTStream/"
 
 i=0
 for l in open(primerf, 'r'):
@@ -39,11 +35,13 @@ for l in open(primerf, 'r'):
         sample = l2[header.index('SampleID')]
         logf = './01-PrimerTrim/' + sample + '.log'
         prefix = './01-PrimerTrim/' + sample
-        cmd = "nice -n1 " + htspath + "hts_Primers -d 0 -l 0 -e 0 -r 2 -x -P " + l2[header.index('Primer1Seq')] + "TGGGG"
-        cmd += " -Q " + l2[header.index('Primer2Seq')] + " -1 " + r1 + " -2 " + r2 + " -L " + logf + " | "
-        cmd += htspath + "hts_NTrimmer -e -AL " + logf + " | "
-        cmd += htspath + "hts_SeqScreener -C -r -x .01 -k 21 -s aberrant_LC.fasta -AL " + logf + " | "
-        cmd += htspath + "hts_QWindowTrim -l -n -q 10 -m 260 -AL " + logf + " | "
-        cmd += htspath + "hts_Overlapper -AL " + logf + " -m 385 -f " + prefix + '\n'
+        cmd =  f"nice -n1 {htspath}hts_Primers -d 0 -l 0 -e 0 -r 2 -x -P {l2[header.index('TSOBarcode')]}"
+        cmd += f" -Q {l2[header.index('TargetSpecificPrimers')]} -1 {r1} -2  {r2} -L {logf} | "
+        #cmd += f"{htspath}hts_Primers -d 4 -l 0 -e 0 -r 1 -P {l2[header.index('TSOAdapter')]} -A {logf} | "
+        cmd += f"{htspath}hts_NTrimmer -e -A {logf} | "
+        cmd += f"{htspath}hts_SeqScreener -C -r -x .01 -k 21 -s aberrant_LC.fasta -A {logf} | "
+        cmd += f"{htspath}hts_QWindowTrim -l -q 10 -A {logf} | "
+        cmd += f"{htspath}hts_Overlapper -A {logf} | " 
+        cmd += f"{htspath}hts_LengthFilter -m 385 -A {logf} -f {prefix}\n"
         outf.write(cmd)
 outf.close()
