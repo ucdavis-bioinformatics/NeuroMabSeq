6bb12d400a85e617790d41daf093717f49309a70
Sam Hunter <shunter@gmail.com>
2020-11-18 10:08:58 -0800

    Split scripts for cleaning and processing

--- setup_pipeline.py
+++ setup_pipeline.py
@@ -12,7 +12,9 @@ ss = csv.DictReader(open("./NeuroMabSeq/SampleSheet.txt", 'r'), delimiter='\t')
 
 # Submit jobs for all pipelines:
 #slurmf = open("submit_slurm.sh", 'w')
-runf = open("run_sequential.sh", 'w')
+#runf = open("run_sequential.sh", 'w')
+runHTSf = open("run_HTS.sh", 'w')  # run HTS, this should be run sequentially
+runProcessingf = open("run_processing.sh", 'w')  # Run processing, can be run in parallel
 
 # Setup plates:
 for plate in ss:
@@ -21,31 +23,24 @@ for plate in ss:
     r1 = r1[0]
     r2 = r1.replace("_R1_", "_R2_")
     s = os.path.join('01-Processing', plate['plate'])
-    os.system('mkdir -p ./' + s + '/00-RawData/')
-    os.system('mkdir -p ./' + s + '/01-PrimerTrimReport/')
-    os.system('mkdir -p ./' + s + '/02-Results/')
-    os.system('ln -s ' + os.path.abspath(r1) + ' ./' + s + '/00-RawData/')
-    os.system('ln -s ' + os.path.abspath(r2) + ' ./' + s + '/00-RawData/')
-    os.system('ln -s ' + os.path.abspath('./NeuroMabSeq/01-build_hts.py') + ' ./' + s + '/')
-    os.system('ln -s ' + os.path.abspath('./NeuroMabSeq/aberrant_LC.fasta') + ' ./' + s + '/')
-    os.system('cp ./NeuroMabSeq/01-PrimerTrimReport/report.RMD ./' + s + '/01-PrimerTrimReport/' + s + "_report.RMD")
-    os.system('ln -s ' + os.path.abspath('./NeuroMabSeq/SMARTindex_well.tsv') + ' ./' + s + '/02-Results/')
-    os.system('cp ./NeuroMabSeq/02-Results/02-Hybridoma-DADA2-analysis.RMD ./' + s + '/02-Results/')
-    os.system('ln -s ' + os.path.abspath('./NeuroMabSeq/03-annotate-results.py') + ' ./' + s + '/')
-    os.system('ln -s ' + os.path.abspath(f'./NeuroMabSeq/{plate["Primers"]} ')  + s + '/')
-    os.system('ln -s ' + os.path.abspath('samlogin.pem') + ' ./' + s + '/')
-
-    #Create run_pipeline.sh with proper settings:
-    with open(f"./{s}/run_pipeline.sh", 'w') as outf:
-        outf.write("#Setup\n")
-        cmd = "aklog\n"
-        outf.write(cmd)
-        cmd = "module load R/3.6.1\n"
-        outf.write(cmd)
-        cmd = "module load hmmer/3.1b2\n"
-        outf.write(cmd)
-        cmd = "source /share/biocore/projects/Trimmer_James_UCD/2019.11.18-Trimmer-Hybridoma-Seq/ANARCI-venv/bin/activate\n"
-        outf.write(cmd)
+    os.system(f'mkdir -p ./{s}/00-RawData/')
+    os.system(f'mkdir -p ./{s}/01-PrimerTrimReport/')
+    os.system(f'mkdir -p ./{s}/02-Results/')
+    os.system(f'ln -s {os.path.abspath(r1)} ./{s}/00-RawData/')
+    os.system(f'ln -s {os.path.abspath(r2)} ./{s}/00-RawData/')
+    os.system(f'ln -s {os.path.abspath('./NeuroMabSeq/hc_primers.fasta')} ./{s}/')
+    os.system(f'ln -s {os.path.abspath('./NeuroMabSeq/lc_primers.fasta')} ./{s}/')
+    os.system(f'ln -s {os.path.abspath('./NeuroMabSeq/01-build_hts.py')} ./{s}/')
+    os.system(f'ln -s {os.path.abspath('./NeuroMabSeq/aberrant_LC.fasta')} ./{s}/')
+    os.system(f'cp ./NeuroMabSeq/01-PrimerTrimReport/report.RMD ./{s}/01-PrimerTrimReport/{s}_report.RMD')
+    os.system(f'ln -s {os.path.abspath('./NeuroMabSeq/SMARTindex_well.tsv')} ./{s}/02-Results/')
+    os.system(f'cp ./NeuroMabSeq/02-Results/02-Hybridoma-DADA2-analysis.RMD ./{s}/02-Results/')
+    os.system(f'ln -s {os.path.abspath('./NeuroMabSeq/03-annotate-results.py')} ./{s}/')
+    os.system(f'ln -s {os.path.abspath(f'./NeuroMabSeq/{plate["Primers"]}')} ./{s}/')
+    os.system(f'ln -s {os.path.abspath('samlogin.pem')} ./{s}/')
+  
+    # Setup cleaning:
+    with(open(f"./{s}/00-run_cleaning.sh", 'w')) as outf:
         outf.write("\n#Build HTStream script\n")
         cmd = f"python 01-build_hts.py {r1} {r2} {plate['Primers']} 01-runHTS.sh\n"
         outf.write(cmd)
@@ -57,6 +52,18 @@ for plate in ss:
         cmd = f"module load R/3.6.1;"
         cmd += f"Rscript -e \"plate='{plate['plate']}';submission='{plate['submissionID']}';"
         cmd += f"rmarkdown::render('./01-PrimerTrimReport/{s}_report.RMD')\"\n"
+        outf.write(cmd)
+    
+    # Create scripts for processing pipeline
+    with(open(f"./{s}/01-run_processing.sh", 'w')) as outf:
+        outf.write("#Setup\n")
+        cmd = "aklog\n"
+        outf.write(cmd)
+        cmd = "module load R/3.6.1\n"
+        outf.write(cmd)
+        cmd = "module load hmmer/3.1b2\n"
+        outf.write(cmd)
+        cmd = "source /share/biocore/projects/Trimmer_James_UCD/2019.11.18-Trimmer-Hybridoma-Seq/ANARCI-venv/bin/activate\n"
         outf.write(cmd)
         # Build ASVs:
         outf.write("\n# Build ASVs:\n")
@@ -81,10 +88,16 @@ for plate in ss:
         cmd = f"rsync -avz -e 'ssh -i samlogin.pem' ./02-Results/02-Hybridoma-DADA2-analysis.html {dest}HTML_Reports/{plate['plate']}_report.html\n"
         outf.write(cmd)
 
-    runf.write(f"echo {plate['plate']}\n")
-    runf.write(f"cd {os.path.abspath(s)}\n")
-    runf.write('bash ./run_pipeline.sh\n')
-    runf.write("cd /share/biocore/projects/Trimmer_James_UCD/Hybridoma-Seq-Processing\n\n")    
+    runHTSf.write(f"echo {plate['plate']}\n")
+    runHTSf.write(f"cd {os.path.abspath(s)}\n")
+    runHTSf.write('bash ./00-run_cleaning.sh\n')
+    runHTSf.write("cd /share/biocore/projects/Trimmer_James_UCD/Hybridoma-Seq-Processing\n\n")
+
+    runProcessingf.write(f"echo {plate['plate']}\n")
+    runProcessingf.write(f"cd {os.path.abspath(s)}\n")
+    runProcessingf.write('bash ./01_run_processing.sh\n')
+    runProcessingf.write("cd /share/biocore/projects/Trimmer_James_UCD/Hybridoma-Seq-Processing\n\n")
+
     #slurmf.write(f"srun -t 1:0:0 -c {ncpu} -n 1 --mem 16000 --partition production -J {plate['plate']} --output slurmout " + f"./{s}/run_pipeline.sh\n")
     #rsync -vrt --no-p --no-g --chmod=ugo=rwX ./03-AnnotatedResults/*.tsv bioshare@bioshare.bioinformatics.ucdavis.edu:/3ksenvfdffie3aj/AnnotatedResults/
     #rsync -vrt --no-p --no-g --chmod=ugo=rwX ./02-Results/*_SampleStatus.tsv bioshare@bioshare.bioinformatics.ucdavis.edu:/3ksenvfdffie3aj/StatusReports/
