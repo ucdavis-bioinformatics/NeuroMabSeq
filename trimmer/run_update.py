83def160845778c81c9cbf6ecf86b2f3a043b478
Keith M <keithgmitchell@ucla.edu>
2021-02-17 12:05:40 -0800

    updates for add gene etc

--- trimmer/run_update.py
+++ trimmer/run_update.py
@@ -8,7 +8,7 @@ import sys
 #directory = sys.argv[1]
 
 is_update = False
-directory = "/Users/keithmitchell/Desktop/Repositories/NeuroMabSeq/data3"
+directory = "/Users/keithmitchell/Desktop/Repositories/NeuroMabSeq/data3/AnnotatedResults"
 
 
 data_upload(is_update, directory) # True for update
